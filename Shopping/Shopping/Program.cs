9194d4d78b6928e46d2199a1c8d724bfc7ce9b97
Dairo Andres <dairoandres115@outlook.com>
2022-03-30 19:05:09 -0500

    database feeder finalized

--- Shopping/Shopping/Program.cs
+++ Shopping/Shopping/Program.cs
@@ -3,23 +3,34 @@ using Shopping.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
-
-builder.Services.AddDbContext<DataContext>(o => {
+builder.Services.AddDbContext<DataContext>(o =>
+{
     o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddTransient<SeedDb>(); //Database feeder
+
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+//Seed database method
+SeedData();
+void SeedData()
+{
+    IServiceScopeFactory? scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+    using (IServiceScope? scope = scopedFactory.CreateScope())
+    {
+        SeedDb? service = scope.ServiceProvider.GetService<SeedDb>();
+        service.SeedAsync().Wait();
+    }
+}
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
