ad871628de9dd618b9e8816d4313916baa27b533
Dairo Andres <dairoandres115@outlook.com>
2022-04-19 20:40:38 -0500

    products management finalized

--- Shopping/Shopping/Data/DataContext.cs
+++ Shopping/Shopping/Data/DataContext.cs
@@ -13,6 +13,9 @@ namespace Shopping.Data
         public DbSet<Country> Countries { get; set; }
         public DbSet<State> States { get; set; }
         public DbSet<City> Cities { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -21,6 +24,8 @@ namespace Shopping.Data
             modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique(); //Country index
             modelBuilder.Entity<State>().HasIndex("Name", "CountryId").IsUnique(); //State index
             modelBuilder.Entity<City>().HasIndex("Name", "StateId").IsUnique(); //City index
+            modelBuilder.Entity<Product>().HasIndex(p => p.Name).IsUnique(); //Product index
+            modelBuilder.Entity<ProductCategory>().HasIndex("ProductId", "CategoryId").IsUnique(); //ProductCategory index
         }
     }
 }
---
+++ Shopping/Shopping/Models/CreateProductViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Models
+{
+    public class CreateProductViewModel : EditProductViewModel
+    {
+        [Display(Name = "Categoría")]
+        [Range(1, int.MaxValue, ErrorMessage = "Debes seleccionar una categoría")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public int CategoryId { get; set; }
+
+        public IEnumerable<SelectListItem> Categories { get; set; }
+
+        [Display(Name = "Foto")]
+#pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
+        public IFormFile? ImageFile { get; set; }
+#pragma warning restore CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
+    }
+}
